dad60ea66adfac549a2e7f4828201a7dd3e8c714
Everton Vanzella <everton_747@hotmail.com>
2020-04-01 15:11:24 -0300

    add router.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const routes = require('./routes/router');
 
 var app = express();
 
@@ -14,7 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', router);
 
 module.exports = app;
